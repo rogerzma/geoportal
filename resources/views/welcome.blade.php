238a5b1f6613f2186e345ea4aa755ce5146eeea4
rogerzma <rogerzma500@gmail.com>
2025-04-21 14:54:02 -0600

    cambios en el mapa de qgis

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,65 +1,19 @@
-<!-- En tu Blade -->
+<!-- filepath: c:\xampp\htdocs\geoportal\resources\views\welcome.blade.php -->
 <!DOCTYPE html>
 <html>
 <head>
     <title>Geoportal de Parcelas</title>
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" />
-    <link rel="stylesheet" href="https://unpkg.com/leaflet-draw@1.0.4/dist/leaflet.draw.css"/>
     <style>
-        #map { height: 100vh; width: 100vw; }
+        iframe {
+            width: 100vw;
+            height: 100vh;
+            border: none;
+        }
     </style>
 </head>
 <body>
-    <div id="map"></div>
-
-    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"></script>
-    <script src="https://unpkg.com/leaflet-draw@1.0.4/dist/leaflet.draw.js"></script>
-
-    <script>
-        const map = L.map('map').setView([22.7709, -102.5832], 7); // Centro en Zacatecas
-
-        // Capa de imagen satelital de Esri
-        L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-            attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye',
-            maxZoom: 19
-        }).addTo(map);
-
-        const drawnItems = new L.FeatureGroup();
-        map.addLayer(drawnItems);
-
-        const drawControl = new L.Control.Draw({
-            edit: {
-                featureGroup: drawnItems
-            },
-            draw: {
-                polygon: true,
-                polyline: false,
-                rectangle: false,
-                circle: false,
-                marker: false,
-                circlemarker: false
-            }
-        });
-        map.addControl(drawControl);
-
-        map.on(L.Draw.Event.CREATED, function (e) {
-            const layer = e.layer;
-            drawnItems.addLayer(layer);
-
-            const geojson = layer.toGeoJSON();
-            console.log("Parcela digitalizada:", JSON.stringify(geojson));
-
-            // Enviar a backend Laravel
-            fetch('/parcelas', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
-                },
-                body: JSON.stringify(geojson)
-            });
-        });
-    </script>
+    <!-- Incrustar el mapa de QGIS2Web -->
+    <iframe src="{{ asset('qgis2web_2025_04_20-21_28_11_136464/index.html') }}"></iframe>
 </body>
 </html>
