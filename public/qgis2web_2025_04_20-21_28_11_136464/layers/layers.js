bf7adc5c9126b9d05498759a324fbe804c35d43b
rogerzma <rogerzma500@gmail.com>
2025-04-21 11:21:39 -0600

    First commit

---
+++ public/qgis2web_2025_04_20-21_28_11_136464/layers/layers.js
@@ -0,0 +1,17 @@
+var wms_layers = [];
+
+
+        var lyr_GoogleHybrid_0 = new ol.layer.Tile({
+            'title': 'Google Hybrid',
+            'type':'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' &nbsp &middot; <a href="https://www.google.at/permissions/geoguidelines/attr-guide.html">Map data ©2015 Google</a>',
+                url: 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}'
+            })
+        });
+
+lyr_GoogleHybrid_0.setVisible(true);
+var layersList = [lyr_GoogleHybrid_0];
