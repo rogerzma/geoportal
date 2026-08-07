b6ebb5c3000ff86070f5bf0417177e56b82f5b5b
rogerzma <rogerzma500@gmail.com>
2026-08-07 14:49:37 -0600

    cambio en el mapa gob

--- resources/views/admin/RegistrarCultivos.blade.php
+++ resources/views/admin/RegistrarCultivos.blade.php
@@ -133,7 +133,7 @@
     
       <div class="row">
         <div class="col-md-8">
-            <h4>¿El cultivo tiene variantes?</h4>
+            <h4>¿El cultivo tiene variedades?</h4>
 
             <label class="radio-inline">
                 <input
@@ -163,7 +163,7 @@
         style="display: none; margin-top: 20px;"
     >
         <div class="col-md-5">
-            <h4>Variantes</h4>
+            <h4>Variedades</h4>
 
             <div id="lista-variantes"></div>
         </div>
